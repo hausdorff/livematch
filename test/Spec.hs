b5d64c57a3d245eb5a72aacb4ad9acdd861ba935
Alex Clemmer <clemmer.alexander@gmail.com>
2017-12-28 23:15:49 -0500

    Add multi-corpus tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -16,16 +16,32 @@ main = hspec $ do
     it "indexes trivial corpus" $ do
       let idx = index ["abc"]
       let target1 = M.singleton 0 (S.singleton 0)
-      let target2 = M.singleton 0 (S.fromList [0])
-      let target3 = M.singleton 0 (S.fromList [1])
-      let target4 = M.singleton 0 (S.fromList [1])
-      let target5 = M.singleton 0 (S.fromList [2])
+      let target2 = M.singleton 0 (S.singleton 1)
+      let target3 = M.singleton 0 (S.singleton 2)
 
       query idx "a" `shouldBe` target1
-      query idx "ab" `shouldBe` target2
-      query idx "b" `shouldBe` target3
-      query idx "bc" `shouldBe` target4
-      query idx "c" `shouldBe` target5
+      query idx "ab" `shouldBe` target1
+      query idx "b" `shouldBe` target2
+      query idx "bc" `shouldBe` target2
+      query idx "c" `shouldBe` target3
+
+      query idx "z" `shouldBe` empty
+      query idx "az" `shouldBe` empty
+      query idx "za" `shouldBe` empty
+      query idx "abcd" `shouldBe` empty
+
+    it "indexes multi-document corpus" $ do
+      let idx = index ["abc", "bcd"]
+      let target1 = M.fromList [(0, S.singleton 1), (1, S.singleton 0)]
+      let target2 = M.fromList [(0, S.singleton 2), (1, S.singleton 1)]
+      let target3 = M.singleton 0 (S.singleton 0)
+      let target4 = M.singleton 1 (S.singleton 0)
+
+      query idx "b" `shouldBe` target1
+      query idx "bc" `shouldBe` target1
+      query idx "c" `shouldBe` target2
+      query idx "ab" `shouldBe` target3
+      query idx "bcd" `shouldBe` target4
 
       query idx "z" `shouldBe` empty
       query idx "az" `shouldBe` empty
